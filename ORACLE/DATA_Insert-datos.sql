336edfde5f96b3628656fa35ebe246d090f47696
aravestg <75281165+aravestg@users.noreply.github.com>
2024-11-05 12:59:12 -0300

    Create DATA_Insert-datos.sql

---
+++ ORACLE/DATA_Insert-datos.sql
@@ -0,0 +1,8 @@
+INSERT INTO empleados (id, nombre, apellido, correo_electronico, salario)
+VALUES (1, 'Juan', 'Pérez', 'juan.perez@ejemplo.com', 50000);
+
+INSERT INTO empleados (id, nombre, apellido, correo_electronico, salario)
+VALUES
+  (2, 'María', 'González', 'maria.gonzalez@ejemplo.com', 55000),
+  (3, 'Carlos', 'López', 'carlos.lopez@ejemplo.com', 48000),
+  (4, 'Ana', 'Martínez', 'ana.martinez@ejemplo.com', 60000);
