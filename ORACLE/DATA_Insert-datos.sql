7f80ffe0ee82b5bd34f759a2243c7a15db54b30d
aravestg <75281165+aravestg@users.noreply.github.com>
2024-11-05 22:10:10 -0300

    Update DATA_Insert-datos.sql

--- ORACLE/DATA_Insert-datos.sql
+++ ORACLE/DATA_Insert-datos.sql
@@ -6,5 +6,3 @@ VALUES
   (2, 'María', 'González', 'maria.gonzalez@ejemplo.com', 55000),
   (3, 'Carlos', 'López', 'carlos.lopez@ejemplo.com', 48000),
   (4, 'Ana', 'Martínez', 'ana.martinez@ejemplo.com', 60000);
-
-COMMIT;
